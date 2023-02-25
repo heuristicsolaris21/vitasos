402882f5dcb90482539181de09bc80c274790fd8
sanjith <sanjith1309@gmail.com>
2023-02-25 17:30:32 +0530

    sanjith

---
+++ app/src/main/java/com/sanjith/vitasos/helper1.java
@@ -0,0 +1,74 @@
+package com.sanjith.vitasos;
+
+public class helper1 {
+    String editname,editnumber,editage,editpassword,editdia,editbp,editdrugs;
+    public helper1(){
+
+    }
+
+    public helper1(String editname, String editnumber, String editage, String editpassword, String editdia, String editbp, String editdrugs) {
+        this.editname = editname;
+        this.editnumber = editnumber;
+        this.editage = editage;
+        this.editpassword = editpassword;
+        this.editdia = editdia;
+        this.editbp = editbp;
+        this.editdrugs = editdrugs;
+    }
+
+    public String getEditname() {
+        return editname;
+    }
+
+    public void setEditname(String editname) {
+        this.editname = editname;
+    }
+
+    public String getEditnumber() {
+        return editnumber;
+    }
+
+    public void setEditnumber(String editnumber) {
+        this.editnumber = editnumber;
+    }
+
+    public String getEditage() {
+        return editage;
+    }
+
+    public void setEditage(String editage) {
+        this.editage = editage;
+    }
+
+    public String getEditpassword() {
+        return editpassword;
+    }
+
+    public void setEditpassword(String editpassword) {
+        this.editpassword = editpassword;
+    }
+
+    public String getEditdia() {
+        return editdia;
+    }
+
+    public void setEditdia(String editdia) {
+        this.editdia = editdia;
+    }
+
+    public String getEditbp() {
+        return editbp;
+    }
+
+    public void setEditbp(String editbp) {
+        this.editbp = editbp;
+    }
+
+    public String getEditdrugs() {
+        return editdrugs;
+    }
+
+    public void setEditdrugs(String editdrugs) {
+        this.editdrugs = editdrugs;
+    }
+}
