95472a8fa69556568e10a44a0adfa9ff6e6502ee
Vigram <vigramkm@gmail.com>
2023-02-26 10:20:10 +0530

    vigram

--- vitcap2/src/main/java/com/sanjith/vitcap2/Vitacap2cappageActivity.java
+++ vitcap2/src/main/java/com/sanjith/vitcap2/Vitacap2cappageActivity.java
@@ -17,33 +17,40 @@ import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Vitacap2cappageActivity extends AppCompatActivity {
     TextView abc;
     String key;
-    DatabaseReference databasereference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://vitaapp-7628c-default-rtdb.firebaseio.com/").child("data");
+    String attributeValue;
+    DatabaseReference databasereference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://vitaapp-7628c-default-rtdb.firebaseio.com/");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vitacap2cappage);
-
-        //DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("data");
-        Query queryUid=databasereference.orderByKey().limitToFirst(1);
-        queryUid.addListenerForSingleValueEvent(new ValueEventListener() {
+        databasereference.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                for (DataSnapshot datas : dataSnapshot.getChildren()) {
-                    key=datas.getKey();
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists() && snapshot.getValue() instanceof HashMap) {
+                    HashMap<String, Object> hashMap = (HashMap<String, Object>) snapshot.getValue();
+                    if (hashMap.get("data") != null) {
+                        String attributeValue = hashMap.get("data").toString();
+                        Toast.makeText(getApplicationContext(), attributeValue, Toast.LENGTH_SHORT).show();
+                        abc=(TextView)findViewById(R.id.textView3);
+                        abc.setText(attributeValue);
+                    }
                 }
             }
 
             @Override
-            public void onCancelled(DatabaseError databaseError) {
-
+            public void onCancelled(@NonNull DatabaseError error) {
+                // Handle error
             }
         });
+
         abc=(TextView)findViewById(R.id.textView3);
-        abc.setText(key);
+        //Toast.makeText(this, "asdf"+attributeValue, Toast.LENGTH_SHORT).show();
+        //abc.setText(attributeValue);
 
 
     }
